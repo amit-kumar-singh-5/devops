fe61de8ad47c220848fb8237e17dea916147f6d3
amit-kumar-singh-5 <61972048+amit-kumar-singh-5@users.noreply.github.com>
2020-07-31 07:29:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable subscription-id {DEFAULT="179f9d13-6ec9-4011-9fd9-9300cdd6e446"}
-variable resource-group-name {DEFAULT="AMIT-POC"}
-variable adf-name {DEFAULT="amitadf"}
+variable subscription-id {DEFAULT=179f9d13-6ec9-4011-9fd9-9300cdd6e446}
+variable resource-group-name {DEFAULT=AMIT-POC}
+variable adf-name {DEFAULT=amitadf}
 
