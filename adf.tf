e2e14027ad6cdcaa2d01676c605429a3620dfe47
amit-kumar-singh-5 <61972048+amit-kumar-singh-5@users.noreply.github.com>
2020-07-31 07:38:41 +0530

    Update adf.tf

--- adf.tf
+++ adf.tf
@@ -2,6 +2,12 @@ provider "azurerm" {
   version = "=2.20.0"
   features {}
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource-group-name
+  location = "Central US""
+}
+
 resource "azurerm_data_factory" "example" {
   name                = var.adf-name
   location            = "Central US"
