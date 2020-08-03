fe6b935fb7e9aeeffaa178c0fb691357f79c4603
amit-kumar-singh-5 <61972048+amit-kumar-singh-5@users.noreply.github.com>
2020-08-03 16:14:52 +0530

    Update adf.tf

--- adf.tf
+++ adf.tf
@@ -4,12 +4,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "amit-poc5"
+  name     = "amit-poc56"
   location = "northeurope"
 }
 
 resource "azurerm_data_factory" "example" {
-  name                = "exampleadfammu7"
+  name                = "exampleadfammu75"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
